849e62c2b42afa59a4dccaefa0adcc91283923a4
johnkinuthiaa <kinuthiajohn122@gmail.com>
2024-10-25 21:14:56 +0300

    init

---
+++ src/main/java/com/sec/secdailycodework/SecDailyCodeWorkApplication.java
@@ -0,0 +1,13 @@
+package com.sec.secdailycodework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecDailyCodeWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecDailyCodeWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sec/secdailycodework/SecDailyCodeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sec.secdailycodework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecDailyCodeWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
